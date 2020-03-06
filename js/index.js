b19a2b8868aec7e3136206ec02e329ccba64d28f
Jadon Guzman <jjkolet25@tutanota.com>
2020-03-06 12:15:57 -0800

    dark mode functionality

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+  container.classList.toggle('dark');
+  toggle.classList.toggle('darkBtn');
+});
