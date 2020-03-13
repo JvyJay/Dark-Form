f9aea060fec4afca4b63065e6c36ab93be808a22
Jadon Guzman <jjkolet25@tutanota.com>
2020-03-13 12:48:06 -0700

    better validation

--- js/index.js
+++ js/index.js
@@ -7,52 +7,90 @@ toggle.addEventListener('click', () => {
   toggle.classList.toggle('darkBtn');
 });
 
-// form elements
+// Form elements
 const form = document.querySelector('#myForm');
 const username = document.querySelector('#username');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const submit = document.querySelector('#formSubmit');
+const password2 = document.querySelector('#password2');
+
+// Submission event handler
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  validate();
+});
+
+const validate = () => {
+  if (username.value === '') {
+    errMsg(username, 'Username is required');
+  } else {
+    successMsg(username);
+  }
+
+  if (email.value === '') {
+    errMsg(email, 'Email is required');
+  } else {
+    successMsg(email);
+  }
+
+  if (password.value === '') {
+    errMsg(password, 'Password is required');
+  } else if (password.value.length <= 6) {
+    errMsg(password, 'Password must be longer than 6 characters');
+  } else {
+    successMsg(password);
+  }
+
+  if (password2.value === '') {
+    errMsg(password2, 'Password confirmation is required');
+  } else if (password2.value !== password.value) {
+    errMsg(password2, 'Password does not match');
+  } else {
+    successMsg(password2);
+    signupSuccess();
+  }
+};
 
 // Error msg
-function errFunc() {
+let fillTheForm = () => {
   const errMsg = document.createElement('p');
   errMsg.classList.add('errMsg');
   errMsg.textContent = 'Please fill in all fields';
-  errMsg.style.cssText = 'color: red; font-weight: bolder;';
+  errMsg.style.cssText = 'color: darkred; font-weight: bolder;';
   form.prepend(errMsg);
-}
+};
+
+const errMsg = (input, msg) => {
+  const formControl = input.parentElement;
+  // Input border color change
+  const border = input;
+  border.style.cssText = 'border: solid 2px darkred;';
+  // Shows error msg
+  const small = formControl.querySelector('small');
+  small.innerText = msg;
+  small.style.cssText = 'visibility: visible;';
+};
 
 // Success msg
-function successMsg() {
+const successMsg = input => {
+  const formControl = input.parentElement;
+  const border = input;
+  border.style.cssText = 'border: solid 2px lightgreen;';
+  // Hides error msg
+  const small = formControl.querySelector('small');
+  small.style.cssText = 'visibility: hidden;';
+};
+
+const signupSuccess = () => {
   const successMsg = document.createElement('p');
-  successMsg.textContent = 'Successfully signed up';
-  successMsg.style.cssText = 'color: green; font-weight: bolder';
+  successMsg.textContent = 'Success!';
+  successMsg.style.cssText = 'color: green; font-weight: bolder;';
   form.prepend(successMsg);
 
-  setTimeout(() => successMsg.remove(), 3000);
-}
-
-// Validate form
-function validate() {
-  const err = document.querySelector('.errMsg');
-  if (username.value == '' || email.value == '' || password.value == '') {
-    errFunc();
-    // Makes sure the errFunc won't create multiple elements
-    errFunc = function() {};
-  } else {
-    successMsg();
-    // Removes errFunc msg from DOM if necessary
-    err.remove();
-  }
-}
-
-// Submission event handler
-submit.addEventListener('click', e => {
-  e.preventDefault();
-  validate();
+  setTimeout(() => successMsg.remove(), 5000);
 
   username.value = '';
   email.value = '';
   password.value = '';
-});
+  password2.value = '';
+};
